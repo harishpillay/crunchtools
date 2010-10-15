c7ed938bf2cad026fc9f658a47c1fb2f1528c260
Scott McCarty <scott.mccarty@gmail.com>
2010-10-15 17:53:14 -0400

    * Updated installer to include testing SM

---
+++ beaver/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Basic checks
+if [ `whoami` != "root" ]
+then
+    echo "You need to be root to install to /usr/local/"
+    exit 1
+fi
+
+if [ ! -d /usr/local/bin ] && [ ! -d /usr/local/etc ]
+then
+    echo "/usr/local/bin or /usr/local/etc does not exist!"
+    exit 1
+fi
+
+# Optional Checks
+keychain=`which keychain`
+
+if [ -e "$keychain" ]
+then
+    echo "Good: Keychain is installed"
+else
+    echo "Bad: You will need to install keychain to do ssh key authentication automatically"
+fi
+
+# Install process
+cp -v beaver_backup.sh /usr/local/bin/
+cp -v beaver_backup.conf /usr/local/bin/
+cp -v beaver_backup.excludes /usr/local/bin/
+cp -v beaver_backup.list /usr/local/bin/
+
+if [ -e /usr/local/bin/beaver_backup.sh ] && \
+   [ -e /usr/local/bin/beaver_backup.conf ] && \
+   [ -e /usr/local/bin/beaver_backup.excludes ] && \
+   [ -e /usr/local/bin/beaver_backup.list ]
+then
+    echo "Success"
+    echo "Now add your servers to /usr/local/bin/beaver_backup.list and configure ssh key authentication"
+fi
